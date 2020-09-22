9226a7d3567907c72d666a62a9644667c6e21c3b
tax25 <andreatax25@gmail.com>
2020-09-22 11:03:27 +0200

    Code comments are now in English, updated README file.

--- Arduino_code/car-code.ino
+++ Arduino_code/car-code.ino
@@ -1,28 +1,27 @@
-// 25 Agosto 2020 Inizio a lavorare a questo codice.
-// L'altro è un po' rotto, per questo ne faccio uno nuovo.
+// 25 Agosto 2020, Starting to work to this code.
 
 // Steering Stuff//
 #include "Servo.h"
 Servo myServo;
 #define pinServo 4
-// Pin dei 2 motori
-	// motore 1
+// motor pins
+	// 1st motor
 	#define pwmA 2
 	#define in1A 22
 	#define in2A 23
 
-	// motore 2
+	// 2nd motor
 	#define pwmB 3
 	#define in1B 24
 	#define in2B 25
 
-// Per la guida performata da una persona
+// For "human" driving
 // i-o joystick:
 #define  pinX A0
 #define  pinY A1
 #define  buttonPin 52
 
-// Collegamenti dei sensori
+// Ultra-sound sensor connections
 #define firstSensor_trigpin 5
 #define firstSensor_echopin 6
 /////////////////////////////
@@ -40,29 +39,32 @@ void setup(){
 
 	myServo.attach(pinServo);
 
-	//Settaggio primo motore//
+	/*First motor setting*/
   	pinMode(pwmA, OUTPUT);
   	pinMode(in1A, OUTPUT);
   	pinMode(in2A, OUTPUT);
-  	//Settaggio secondo motore//
+  /*Second motor setting*/
   	pinMode(pwmB, OUTPUT);
   	pinMode(in1B, OUTPUT);
   	pinMode(in2B, OUTPUT);
 
-  	//Settaggio sensori ad ultrasuoni
-  	//Primo
+  	//Ultra-sound sensors setting
+  	//First
   	pinMode(firstSensor_trigpin, OUTPUT);
   	pinMode(firstSensor_echopin, INPUT);
-  	//Secondo
+  	//Second
   	pinMode(secondSensor_trigpin, OUTPUT);
   	pinMode(secondSensor_echopin, INPUT);
-  	//Terzo
+  	//Third
   	pinMode(thirdSensor_trigPin, OUTPUT);
   	pinMode(thirdSensor_echoPin, INPUT);
-
+		//Fourth
+		pinMode(fourthSensor_trigPin, OUTPUT);
+		pinMode(fourthSensor_echoPin, INPUT);
 }
 
-#define val 0.03438
+#define val 0.03438 // To know what this constant is check out the README file
+										// in the "define val" zone.
 
 void loop(){
 
@@ -75,28 +77,28 @@ void loop(){
 void set_motor_speed_and_steering(int pin1, int pin2, int trigPin1, int echoPin1, int trigPin2, int echoPin2, int trigPin3, int echoPin3, int trigPin4, int echoPin4) /*-> None*/{
 	bool going_forward = true;
 
-	//Leggo i valori che arrivano dal joystick per ricavare velocità e sterzo.
-	int _speed = analogRead(pinY); //L'asse Y è l'asse della velocità
-	int _steering = analogRead(pinX); //L'asse X è l'asse dello sterzo
+	// Reading the values returned by the joystick to calculate speed and steering
+	int _speed = analogRead(pinY); // Y axis is the acceleration axis
+	int _steering = analogRead(pinX); // X axis is the steering axis
 
-	//Calcolo le distanze dai vari sensori:
-//Primo sensore (davanti)
+	// Calculating the distances of the sensors
+//1st sensor (front)
 	digitalWrite(trigPin1, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin1, LOW);
-    float returnedTime1 = pulseIn(echoPin1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin1, LOW);
+  float returnedTime1 = pulseIn(echoPin1, HIGH);
 	float distanza_davanti = val * returnedTime1 / 2;
 
 	delayMicroseconds(10);
-//Secondo sensore (dietro)
-    digitalWrite(trigPin2, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin2, LOW);
-    float returnedTime2 = pulseIn(echoPin2, HIGH);
+//2nd sensor (behind)
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW);
+  float returnedTime2 = pulseIn(echoPin2, HIGH);
 	float distanza_dietro = val * returnedTime2 / 2;
 
 	delayMicroseconds(10);
-//Terzo sensore (destra)
+//3rd sensor (right)
 	digitalWrite(trigPin3, HIGH);
 	delayMicroseconds(10);
 	digitalWrite(trigPin3, LOW);
@@ -104,7 +106,7 @@ void set_motor_speed_and_steering(int pin1, int pin2, int trigPin1, int echoPin1
 	float distanza_destra = val * returnedTime3 / 2;
 
 	delayMicroseconds(10);
-//Quarto sensore (sinistra)
+//4th sensor (left)
 	digitalWrite(trigPin4, HIGH);
 	delayMicroseconds(10);
 	digitalWrite(trigPin4, LOW);
@@ -112,25 +114,14 @@ void set_motor_speed_and_steering(int pin1, int pin2, int trigPin1, int echoPin1
 	float distanza_sinistra = val * returnedTime4 / 2;
 
 
+	// Taking the values returned by the joystick and "translating" them from
+	// 0-1023 to (negative)100-100. Look at the README file for more infos,
+	// "Translating joystick values from 0/1023 to -100/100 and to 45/135" section.
+	int effective_speed = map(_speed, 0, 1023, 100, -100);
 
-	int effective_speed = map(_speed, 0, 1023, 100, -100); // Cambio i loro valori da 0 a 1023 -> 100 a -100.
-														  // Abbiamo scelto 100 e -100 per avere la velocità massima di 100
-														  // sia in forward che in backward, ma non avere una velocità di
-														  // di punta degna di un 747.
-	int effective_steering = map(_steering, 0, 1023, 45, 135); //Invece per il servo abbiamo deciso di mettere come valore
-														   // minimo 45 gradi, che è il massimo sterzo a sinistra, e
-														   // 135 gradi sono il massimo sterzo a destra, esattamente 90 gradi
-														   // di sterzo. Nella posizione "di riposo" siamo a 90 gradi.
+	int effective_steering = map(_steering, 0, 1023, 45, 135);
 
-	/*
-		Certe volte potrebbe servire mettere un numero in più nella funzione
-		map() per avere l'esatto valore massimo che vogliamo.
-		Infatti potrebbe accadere che mettiate come conversione da 0 a 100 per
-		esempio, e il massimo numero che ricevete sia 99. Non so da cosa venga
-		causata la faccienda, ma quando mi è acccaduto ho risolto in questo modo.
-	*/
-
-
+	//some debugging things
 	//Serial.println("Steering: " + String(effective_steering));
 	//Serial.println(effective_speed);
 	if (effective_speed < 0){
@@ -142,10 +133,13 @@ void set_motor_speed_and_steering(int pin1, int pin2, int trigPin1, int echoPin1
 		digitalWrite(in2B, HIGH);
 
 		effective_speed *= -1;
-		// Moltiplico per -1 la velocità che arriva nel caso sia negativa,
-		// perché al driver non piacciono i numeri negativi e spara il
-		// motore a velocità massima (250).
+
+		// Multiplying for -1 the speed in case it's negative because if the motor
+		// driver receives a negative value, it assigns speed 250 to the motor.
+		// When the speed is negative, the car goes in reverse.
+
 	}else{
+		// some debugging things
 		//Serial.println("\nBuonasera adesso il valore di going_forward e' uguale a 1!");
 		going_forward = true;
 		digitalWrite(in1A, HIGH);
@@ -154,27 +148,28 @@ void set_motor_speed_and_steering(int pin1, int pin2, int trigPin1, int echoPin1
 		digitalWrite(in2B, LOW);
 	}
 
+	//Some debugging things
 	//Serial.println("Speed: " + String(effective_speed) + " Questo è il valore di going_forward: " + String(going_forward));
 	//Serial.println(effective_steering);
 	/*
-		IMPORTANTE:
-		Se effective_steering è maggiore di 90 allora la macchina sta girando a destra, altrimenti a sinistra.
+		IMPORTANT:
+		If "effective_steering" is greater than 90 the car is steering right, otherwise it's steering left.
 	*/
 	if ((distanza_davanti >= 15.0)&&(going_forward==1)){
-
+		//Some debugging things
 		//Serial.println("Valore going_forward: " + String(going_forward) + "STO ANDANDO AVANTI MADERFADERS!!!!");
 		//Serial.println("Valore Sterzo: " + String(effective_steering) + "Questa è la distanza destra: " + String(distanza_destra));
 
 		myServo.write(effective_steering);
 
 		if (effective_steering == 90){
-			//Sto andando avanti
+			//Going forward
 			Serial.println("Sto andando avanti" + String(effective_speed));
 			analogWrite(pin1, effective_speed);
 			analogWrite(pin2, effective_speed);
 		}
 		if ((effective_steering > 90)&&(distanza_destra >= 15.0)){
-			//Sto girando a destra
+			//Steering right
 			Serial.println("Sto girando a destra");
 			analogWrite(pin1, effective_speed);
 			analogWrite(pin2, effective_speed);
@@ -184,8 +179,8 @@ void set_motor_speed_and_steering(int pin1, int pin2, int trigPin1, int echoPin1
 		}
 
 		if ((effective_steering < 90)&&(distanza_sinistra >= 15.0)){
-			//Sto girando a sinistra
-			Serial.println("Sto girando a sinistra");
+			//Steering left
+			/*Debugging things*/Serial.println("Sto girando a sinistra");
 			analogWrite(pin1, effective_speed);
 			analogWrite(pin2, effective_speed);
 		}else{
@@ -195,14 +190,14 @@ void set_motor_speed_and_steering(int pin1, int pin2, int trigPin1, int echoPin1
 
 
 	}else if((distanza_dietro >= 15.0)&&(going_forward==0)){
-
+		//Some debugging things
 		//Serial.println("Valore going_forward: " + String(going_forward) + "STO ANDANDO INDIETRO MADERFADERS!!!!");
 
 		myServo.write(effective_steering);
 
 		if ((effective_steering > 90)&&(distanza_destra >= 15.0)){
 
-			Serial.println("Sto facendo retro verso destra");
+			/*Debugging things*/Serial.println("Sto facendo retro verso destra");
 
 			analogWrite(pin1, effective_speed);
 			analogWrite(pin2, effective_speed);
@@ -213,8 +208,8 @@ void set_motor_speed_and_steering(int pin1, int pin2, int trigPin1, int echoPin1
 
 		if ((effective_steering < 90)&&(distanza_sinistra >= 15.0)){
 
-			Serial.println("Sto facendo retro verso sinistra");
-
+			/*Debugging things*/Serial.println("Sto facendo retro verso sinistra");
+			//Some debugging things
 			//Serial.println("sinistra occupata" + String(distanza_sinistra));
 			analogWrite(pin1, effective_speed);
 			analogWrite(pin2, effective_speed);
@@ -224,7 +219,7 @@ void set_motor_speed_and_steering(int pin1, int pin2, int trigPin1, int echoPin1
 		}
 
 	}else {
-		Serial.println("Zona occupata!");
+		/*Debugging things*/Serial.println("Zona occupata!");
 		analogWrite(pin1, 0);
 		analogWrite(pin2, 0);
 	}
--- README.md
+++ README.md
@@ -3,5 +3,32 @@
 This is the code for an Arduino project that I started.
 The project is still in development, so the code will be modified over and over again over the next
 few days or months.
-You can notice that the comments in the code are in Italian, because I'm Italian
-I'll change them to English as soon as possible
+
+## define val (row 66)
+The value "val" is the sound speed at a temperature of 20° Celsius in centimeters per microsecond.
+We use it to calculate the distance between the sensor and an obstacle.
+To calculate the distance between the sensor and the obstacle we have to
+multiply the speed of sound (that for now is "val") for the time returned by
+the pulseIn func divided by 2, like this: val * returnedTime/2.
+
+I plan to replace "val" with the real speed of sound for the temperature in the
+room the robot is in, to do it I think I'll use the DHT22 temperature & humidity
+sensor to calculate the speed of sound.
+We can calculate the speed of sound by following this formula:
+sound_speed = 0.03314 + 0.000062 * temperature (the values are in cm/microsecond).
+
+## Translating joystick values from 0/1023 to -100/100 and to 45/135
+The joystick returns 0 to 1023 values from the potentiometers it has, but
+We want to use the joystick to control the speed of the motor, and the motor driver
+only accepts values from 0 to 255. To be able to use the joystick to control the
+speed we have to "translate" them to min = 0 and max = 255, to do this we
+use the function "map".
+
+We do the same thing for the steering too.
+45° is the maximum left steering, 90° the car is going straight and 135° is the
+maximum right steering.
+
+ATTENTION: sometimes to have the maximum value you want, after the "translation"
+you have to add a number in the map function. 
+Eg: map(_speed, 0, 1023, 100, -100); could give you 99 and -99 as max values.
+to avoid this you can just write 101. map(_speed, 0, 1023, 101, -101);
